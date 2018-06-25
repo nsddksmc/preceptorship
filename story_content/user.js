be764cbac45f72566f65609848af8ad57f3bb80c
maltous <35376350+maltous@users.noreply.github.com>
2018-06-25 21:13:08 +0300

    preceptorship

---
+++ story_content/user.js
@@ -0,0 +1,812 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6nQaIgLs1UJ":
+        Script1();
+        break;
+      case "5vHTE6Qtypa":
+        Script2();
+        break;
+      case "5uIzAPR5EEM":
+        Script3();
+        break;
+      case "5XvPVJw2DDo":
+        Script4();
+        break;
+      case "5ko4JDnATqL":
+        Script5();
+        break;
+      case "6HUBQSun1HF":
+        Script6();
+        break;
+      case "6g4eyyf8evp":
+        Script7();
+        break;
+      case "5sHnOxc4Yln":
+        Script8();
+        break;
+      case "67x5gWhfnW6":
+        Script9();
+        break;
+      case "6IHasPb8WXK":
+        Script10();
+        break;
+      case "6OKffLMKmUo":
+        Script11();
+        break;
+      case "63q7Rwbarae":
+        Script12();
+        break;
+      case "5ix9Xec92s0":
+        Script13();
+        break;
+      case "6djTeZRCIr3":
+        Script14();
+        break;
+      case "6RiHNhi2hcW":
+        Script15();
+        break;
+      case "6IRUcJHIaXO":
+        Script16();
+        break;
+      case "5lXHg7Fk1rD":
+        Script17();
+        break;
+      case "680o3MhbetP":
+        Script18();
+        break;
+      case "6f8zWvxDdPU":
+        Script19();
+        break;
+      case "6pgjH2hEMem":
+        Script20();
+        break;
+      case "6UGB5pbV6Xx":
+        Script21();
+        break;
+      case "6ODPztPNBcE":
+        Script22();
+        break;
+      case "65gZFZgKWZE":
+        Script23();
+        break;
+      case "6p5Y8t0qnG6":
+        Script24();
+        break;
+      case "5w7LG6sNpNH":
+        Script25();
+        break;
+      case "6Rj5h28sw6N":
+        Script26();
+        break;
+      case "694QMMvC7d7":
+        Script27();
+        break;
+      case "6dIqD4tKn1Y":
+        Script28();
+        break;
+      case "5oSga7NF0aD":
+        Script29();
+        break;
+      case "5xdmHY5KL0N":
+        Script30();
+        break;
+      case "6QRHXd7wtY3":
+        Script31();
+        break;
+      case "5wLIpCz9TRc":
+        Script32();
+        break;
+      case "6CuPMPUCJ6r":
+        Script33();
+        break;
+      case "5VLGzYxgdX3":
+        Script34();
+        break;
+      case "66mImYM6jsO":
+        Script35();
+        break;
+      case "5XENqMWZ0nL":
+        Script36();
+        break;
+      case "6mjDDe2dsET":
+        Script37();
+        break;
+      case "5dWD36b4lH2":
+        Script38();
+        break;
+      case "6dbD0W7pwfN":
+        Script39();
+        break;
+      case "6U3JLWtOVr3":
+        Script40();
+        break;
+      case "6V0GzHfGk6h":
+        Script41();
+        break;
+      case "64qkIWGziCK":
+        Script42();
+        break;
+      case "6ABr3RCCht5":
+        Script43();
+        break;
+      case "5VP6QIh5Wz0":
+        Script44();
+        break;
+      case "5w9LAhgbPXl":
+        Script45();
+        break;
+      case "6PDuh3wHGbb":
+        Script46();
+        break;
+      case "6CQ7tzXqKLh":
+        Script47();
+        break;
+      case "5zCF2WWtAS1":
+        Script48();
+        break;
+      case "5Xqwm8CnXxZ":
+        Script49();
+        break;
+      case "5i74IAa4mTF":
+        Script50();
+        break;
+      case "6j2FknpgUhN":
+        Script51();
+        break;
+      case "5r9M7qgnuo5":
+        Script52();
+        break;
+      case "6YwGoX6BcsV":
+        Script53();
+        break;
+      case "6SAKEYunqlK":
+        Script54();
+        break;
+      case "6ZOw6WSurzn":
+        Script55();
+        break;
+      case "6FOpAV1whV4":
+        Script56();
+        break;
+      case "5k3bQDmLO6x":
+        Script57();
+        break;
+      case "5s0FfwJGaHj":
+        Script58();
+        break;
+      case "6rD8cPfWrGI":
+        Script59();
+        break;
+      case "6PofqwJztLu":
+        Script60();
+        break;
+      case "6V91buQoYJ7":
+        Script61();
+        break;
+      case "6aVEmWE4yg6":
+        Script62();
+        break;
+      case "5ZIb26YvoYC":
+        Script63();
+        break;
+      case "6HTrdI1HgZn":
+        Script64();
+        break;
+      case "60ee1zGd7Lk":
+        Script65();
+        break;
+      case "5naApVOKtAt":
+        Script66();
+        break;
+      case "65azzgn4oaV":
+        Script67();
+        break;
+      case "6Tp1J7U4cBQ":
+        Script68();
+        break;
+      case "6Qs2zb84QXJ":
+        Script69();
+        break;
+      case "60GB2zyV4GR":
+        Script70();
+        break;
+      case "5sUFYNKAbK5":
+        Script71();
+        break;
+      case "5UoauZrOuCN":
+        Script72();
+        break;
+      case "6qOc93LqLUX":
+        Script73();
+        break;
+      case "5qlFWtjawV9":
+        Script74();
+        break;
+      case "5saoDWPhTUq":
+        Script75();
+        break;
+      case "6AX78V9TLKf":
+        Script76();
+        break;
+      case "6VOKDhVTNb1":
+        Script77();
+        break;
+      case "5m3Wn1pdZ8z":
+        Script78();
+        break;
+      case "62UEk7ikW90":
+        Script79();
+        break;
+      case "6pOlL4v9g8W":
+        Script80();
+        break;
+      case "5cmOcFNqeFy":
+        Script81();
+        break;
+      case "6qvP7bLvs9p":
+        Script82();
+        break;
+      case "6PrjC1DNGi0":
+        Script83();
+        break;
+      case "6eWglGmnLRD":
+        Script84();
+        break;
+      case "5V0tV0G44om":
+        Script85();
+        break;
+      case "6BI8hzvIeat":
+        Script86();
+        break;
+      case "5r1fzZv0cem":
+        Script87();
+        break;
+      case "6krDZxA2Uea":
+        Script88();
+        break;
+      case "6IS6BdpEib6":
+        Script89();
+        break;
+      case "5wUEeypHe1j":
+        Script90();
+        break;
+      case "5kFdgaJQEoD":
+        Script91();
+        break;
+      case "61KBNshTQwc":
+        Script92();
+        break;
+      case "63yRVltkm2S":
+        Script93();
+        break;
+      case "6acmKAGIThl":
+        Script94();
+        break;
+      case "6QQ0Df0Rmxf":
+        Script95();
+        break;
+      case "65ZEYTDuIPS":
+        Script96();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script6()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script8()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script10()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script11()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script12()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script13()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script14()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script15()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script16()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script17()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script18()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script19()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script20()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script21()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script22()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script23()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script24()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script25()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script26()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script27()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script28()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script29()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script30()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script31()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script32()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script33()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script34()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script35()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script36()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script37()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script38()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script39()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script40()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script41()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script42()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script43()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script44()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script45()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script46()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script47()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script48()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script49()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script50()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script51()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script52()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script53()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script54()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script55()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script56()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script57()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script58()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script59()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script60()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script61()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script62()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script63()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script64()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script65()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script66()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script67()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script68()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script69()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script70()
+{
+  var player = GetPlayer();
+var email="nsdd_cne@ksmc.med.sa";
+var subject=player.GetVar("Name");
+var body_start="Dear Instructor,";
+var mailto_link='mailto:'+email+'?subject='+subject+'&body='+body_start;
+win=window.open(mailto_link,'emailWin');
+}
+
+function Script71()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script72()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script73()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script74()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script75()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script76()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script77()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script78()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script79()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script80()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script81()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script82()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script83()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script84()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script85()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script86()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script87()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script88()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script89()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script90()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script91()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script92()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script93()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script94()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=month + "/" + day + "/" + year
+
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
+function Script95()
+{
+  $("#tab-customlink").show();
+}
+
+function Script96()
+{
+  window.print();
+}
+
